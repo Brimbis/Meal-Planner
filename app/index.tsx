9f5b915eec41419e90281810b69232a63aa8bcc3
gkent8 <gavincolekent@gmail.com>
2024-09-18 13:49:16 -0500

    switch to login added

--- app/index.tsx
+++ app/index.tsx
@@ -1,129 +1,141 @@
-import { Text, SafeAreaView, StyleSheet, TextInput, Button, View } from 'react-native';
-import react, {useState} from "react"
+import {
+  Text,
+  SafeAreaView,
+  TextInput,
+  Button,
+  StyleSheet,
+  View,
+} from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
-    const [state, setState] = useState({
-      firstName: '', 
-      lastName: '', 
-      email: '',
-      password: '', 
-      });
-    const [greeting, setGreeting] = useState('');
-    const [isSuccess, setIsSuccess] = useState(false); // success flag
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [greeting, setGreeting] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
+  const [isComplete, setIsComplete] = useState(false);
 
-    const handleSwitchLogin = () => {
-      
+  const handlePress = () => {
+    if (firstName && lastName && email && password) {
+      setIsComplete(true);
+      setGreeting(
+        `Welcome, ${firstName}! Thank you for Registering an account with us!`
+      );
+    } else {
+      setIsComplete(false);
+      setGreeting(`Please fill in all fields!`);
+      setIsLogin(false);
     }
+  };
 
-    const handlePress = () => {
-      const { firstName, lastName, email, password } = state;
+  const handleSwitchLogin = () => {
+    setIsLogin(true);
+  };
 
-      if (firstName && lastName && email && password) {
-        setGreeting(`Hello, ${firstName} ${lastName}! You have successfully registered your account!`);
-        setIsSuccess(true); // Set success to true
-      } else {
-        setGreeting('Please fill in all fields.');
-        setIsSuccess(false); // Set success to false
-      }
-    };
-    // Registration
-    // First Name, Last Name, Email, Password
-    // If Fields are complete then say registration successful
+  if (isLogin == false) {
     return (
       <SafeAreaView style={styles.container}>
-      <Text style={styles.title}> Sign Up </Text>
-      <View style={styles.separator}/>
-      <View style={styles.center}>
+        <Text style={styles.title}> Register Now </Text>
         <TextInput
-          style={styles.input}
-          autoCapitalize={'words'}
-          value = {state.firstName}
-          onChangeText={text => setState(prevState => ({ ...prevState, firstName: text }))}
+          style={styles.textInput}
+          onChangeText={(text) => setFirstName(text)}
+          value={firstName}
           placeholder="First Name"
         />
-        <View style={styles.separator}/>
         <TextInput
-          style={styles.input}
-          autoCapitalize={'words'}
-          value = {state.lastName}
-          onChangeText={text => setState(prevState => ({ ...prevState, lastName: text }))}
+          style={styles.textInput}
+          onChangeText={(text) => setLastName(text)}
+          value={lastName}
           placeholder="Last Name"
         />
-        <View style={styles.separator}/>
         <TextInput
-          style={styles.input}
-          autoCapitalize={'words'}
-          value = {state.email}
-          onChangeText={text => setState(prevState => ({ ...prevState, email: text }))}
+          style={styles.textInput}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
           placeholder="Email"
         />
-        <View style={styles.separator}/>
         <TextInput
-          style={styles.input}
-          autoCapitalize={'words'}
-          value = {state.password}
-          onChangeText={text => setState(prevState => ({ ...prevState, password: text }))}
+          style={styles.textInput}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
           placeholder="Password"
         />
+        <View style={styles.buttonContainer}>
+          <Button title="Sign Up!!" onPress={handlePress} />
         </View>
-        <View style={styles.separator}/>
-        <View style={styles.center}>
-          <View style={styles.loginBtn}>
-            <Button title="SIGN UP" onPress={handlePress} />
-          </View>
+        <View style={styles.buttonContainer}>
+          <Button title="Switch to Login" onPress={handleSwitchLogin} />
         </View>
-        <View style={styles.center}>
-          <View style={styles.loginBtn}>
-            <Button title="SWITCH TO LOGIN" onPress={handleSwitchLogin} />
-          </View>
-        </View>
-        <Text style={[styles.paragraph, { color: isSuccess ? 'green' : 'red' }]}>
+        <Text
+          style={[styles.paragraph, { color: isComplete ? "green" : "red" }]}
+        >
           {greeting}
         </Text>
       </SafeAreaView>
-  );
+    );
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}> Sign In </Text>
+
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          placeholder="Email"
+        />
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          placeholder="Password"
+        />
+        <View style={styles.buttonContainer}>
+          <Button title="Login In!!" onPress={handlePress} />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button title="Back to sign up!" onPress={handlePress} />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 4,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  input: {
-    height: 40,
-    width: 400, 
-    borderWidth: 1,
-    padding: 10,
-    justifyContent: 'center',
-  }, 
-  title: {
     fontWeight: "bold",
-    fontSize: 24,
-    marginBottom: 10,
-    textAlign: 'center'
-    },
-  center: {
-    alignItems: 'center',
-    marginVertical: 8,
+    textAlign: "center",
   },
-  loginBtn: {
-    width: 200, 
-    textAlign: 'center'
-    },
-  view: {
-    marginVertical: 8, 
-    padding: 20, 
-    },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
+  textInput: {
+    height: 40,
+    width: "75%",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginHorizontal: "auto",
+    marginVertical: 10,
+    padding: 10,
+  },
+
+  title: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+  buttonContainer: {
+    height: 40,
+    width: "40%",
+    marginHorizontal: "auto",
   },
 });
