09c7f12b2223dd88b92dbff6572a18d3fead18eb
BHurt <brandonhurt208@gmail.com>
2024-09-22 14:44:02 -0500

    Login Formatting

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,7 @@ import {
   View,
 } from "react-native";
 import React, { useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   const [firstName, setFirstName] = useState("");
@@ -59,74 +60,118 @@ export default function App() {
   if (isLogin == false) {
     return (
       <SafeAreaView style={styles.container}>
+      <LinearGradient
+        colors={["#6A9C89", "#16423C"]}
+        dither={true}
+        locations={[0.1, 0.8]}
+        start={[0.1, 0.2]}
+        style={styles.linearGradient}
+      >
         <Text style={styles.title}> Register Now </Text>
         <View style={styles.separator}/>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => setFirstName(text)}
-          value={firstName}
-          placeholder="First Name"
-        />
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => setLastName(text)}
-          value={lastName}
-          placeholder="Last Name"
-        />
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="Email"
-        />
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          placeholder="Password"
-        />
+        <View style={styles.inputContainer}>
+        <Text style={styles.smallTitle}>First Name</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => setFirstName(text)}
+            value={firstName}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+        <Text style={styles.smallTitle}>Last Name</Text>
+          <TextInput
+            style={styles.textInput}
+            selectionColor="white"
+            onChangeText={(text) => setLastName(text)}
+            value={lastName}
+          />
+        </View>
+          <View style={styles.inputContainer}>
+          <Text style={styles.smallTitle}>Email</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+        <Text style={styles.smallTitle}>Password</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+        </View>
         <View style={styles.separator}/>
         <View style={styles.buttonContainer}>
-          <Button title="Sign Up" onPress={handlePress} />
+          <Button
+            title="Sign Up"
+            onPress={handlePress}
+            color="#0D2A26"
+            />
         </View>
         <View style={styles.buttonContainer}>
-          <Button title="Switch to Login" onPress={handleSwitchLogin} />
+          <Button
+            title="Switch to Login"
+            onPress={handleSwitchLogin}
+            color="#0D2A26"
+            />
         </View>
         <Text
           style={[styles.paragraph, { color: isComplete ? "green" : "red" }]}>
           {greeting}
         </Text>
+        </LinearGradient>
       </SafeAreaView>
     );
   } else {
     return (
       <SafeAreaView style={styles.container}>
-        <Text style={styles.title}> Sign In </Text>
-        <View style={styles.separator}/>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="Email"
-        />
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          placeholder="Password"
-        />
-        <View style={styles.separator}/>
-        <View style={styles.buttonContainer}>
-          <Button title="Login" onPress={handlePress} />
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <Button title="Switch to Sign Up" onPress={handleSwitchLogin} />
-        </View>
-        <Text
-          style={[styles.paragraph, { color: isComplete ? "green" : "red" }]}>
-          {greeting}
-        </Text>
+        <LinearGradient
+        colors={["#6A9C89", "#16423C"]}
+        dither={true}
+        locations={[0.1, 0.8]}
+        start={[0.1, 0.2]}
+        style={styles.linearGradient}
+        >
+          <Text style={styles.title}> Sign In </Text>
+          <View style={styles.separator}/>
+          <View style={styles.inputContainer}>
+            <Text style={styles.smallTitle}>Email</Text>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={(text) => setEmail(text)}
+              value={email}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+          <Text style={styles.smallTitle}>Password</Text>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={(text) => setPassword(text)}
+              value={password}
+            />
+          </View>
+          <View style={styles.separator}/>
+          <View style={styles.buttonContainer}>
+            <Button 
+              title="Login" 
+              onPress={handlePress}
+              color="#0D2A26"
+              />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button 
+              title="Switch to Sign Up" 
+              onPress={handleSwitchLogin}
+              color="#0D2A26"
+              />
+          </View>
+          <Text
+            style={[styles.paragraph, { color: isComplete ? "green" : "gray" }]}>
+            {greeting}
+          </Text>
+        </LinearGradient>
       </SafeAreaView>
     );
   }
@@ -136,30 +181,51 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    backgroundColor: "#ecf0f1",
+    backgroundColor: "#16423C",
     padding: 8,
   },
+
   paragraph: {
     margin: 24,
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
   },
+
+  linearGradient: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
   textInput: {
     height: 40,
     width: 400,
-    borderColor: "gray",
+    borderColor: "white",
     borderWidth: 1,
     marginHorizontal: "auto",
     marginVertical: 10,
     padding: 10,
   },
 
+  inputContainer: {
+    width: 400,
+    marginHorizontal: "auto",
+    fontFamily: "Inter", 
+  },
+
   title: {
     textAlign: "center",
     fontSize: 20,
     fontWeight: "bold",
+    color: "white", 
   },
+  smallTitle: {
+    fontSize: 15, 
+    fontWeight: "bold",
+    color: "white",  
+  }, 
 
   buttonContainer: {
     height: 40,
